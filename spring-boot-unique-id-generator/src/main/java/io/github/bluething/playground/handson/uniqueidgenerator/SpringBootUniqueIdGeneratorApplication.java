3dedb1a8a665da305d62366ca846c7994e69bea3
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-18 22:10:52 +0700

    Init

---
+++ spring-boot-unique-id-generator/src/main/java/io/github/bluething/playground/handson/uniqueidgenerator/SpringBootUniqueIdGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.handson.uniqueidgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUniqueIdGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUniqueIdGeneratorApplication.class, args);
+    }
+
+}
---
+++ spring-boot-unique-id-generator/src/test/java/io/github/bluething/playground/handson/uniqueidgenerator/SpringBootUniqueIdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.handson.uniqueidgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUniqueIdGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
