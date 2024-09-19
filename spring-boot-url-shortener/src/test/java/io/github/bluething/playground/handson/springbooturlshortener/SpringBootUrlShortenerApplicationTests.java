29c302eb0cf15e2c16dd902afdb4e1d4ae6df565
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-19 22:21:25 +0700

    new module

---
+++ spring-boot-url-shortener/src/test/java/io/github/bluething/playground/handson/springbooturlshortener/SpringBootUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.handson.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
