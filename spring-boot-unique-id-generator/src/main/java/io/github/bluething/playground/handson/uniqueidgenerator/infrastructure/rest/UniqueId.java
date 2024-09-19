4c31ef15d94ff4028f16060e5465a7d39b8405dd
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-19 16:24:30 +0700

    Allow cross origin access

--- spring-boot-unique-id-generator/src/main/java/io/github/bluething/playground/handson/uniqueidgenerator/infrastructure/rest/UniqueId.java
+++ spring-boot-unique-id-generator/src/main/java/io/github/bluething/playground/handson/uniqueidgenerator/infrastructure/rest/UniqueId.java
@@ -3,11 +3,13 @@ package io.github.bluething.playground.handson.uniqueidgenerator.infrastructure.
 import io.github.bluething.playground.handson.uniqueidgenerator.domain.IdGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 class UniqueId {
 
     private final IdGenerator idGenerator;
