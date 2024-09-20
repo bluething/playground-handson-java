4380a7430c1e222e5dff7472c74c8564bce928c6
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-20 23:39:03 +0700

    Add support for database

---
+++ spring-boot-url-shortener/schema.sql
@@ -0,0 +1,13 @@
+create table if not exists url
+(
+    id                    bigserial primary key,
+    long_url              varchar(255),
+    short_url             varchar(255),
+    created_date          timestamp        not null default current_timestamp,
+    updated_date          timestamp,
+    );
+
+create index url_long_url
+    on url (long_url);
+create index url_short_url
+    on url (short_url);
