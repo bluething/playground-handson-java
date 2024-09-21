5da76d968919123ee3c9bd5b6b563a9e877333a1
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-21 14:57:22 +0700

    Add failed test

--- spring-boot-url-shortener/src/test/integration/java/io/github/bluething/playground/handson/springbooturlshortener/UrlShortenerEndpointTest.java
+++ spring-boot-url-shortener/src/test/integration/java/io/github/bluething/playground/handson/springbooturlshortener/UrlShortenerEndpointTest.java
@@ -2,12 +2,14 @@ package io.github.bluething.playground.handson.springbooturlshortener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.bluething.playground.handson.springbooturlshortener.infrastructure.rest.LongUrlPayload;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -22,6 +24,8 @@ class UrlShortenerEndpointTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @DisplayName("When we generate new shorter url, then the system must return 301 Http status code")
     @Test
@@ -31,5 +35,18 @@ class UrlShortenerEndpointTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(url)))
                 .andExpect(status().isMovedPermanently());
+    }
+
+    @DisplayName("When we generate new shorter url, then a new row must be written in database")
+    @Test
+    void generateShorterUrlMustWriteOneRowInDb() throws Exception {
+        LongUrlPayload url = new LongUrlPayload("https://github.com/bluething");
+        mockMvc.perform(put("/api/v1/data/shorten")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(url)))
+                .andReturn();
+
+        Integer numOfRows = jdbcTemplate.queryForObject("select count(*) from url where long_url=?", Integer.class, url.url());
+        Assertions.assertEquals(1, numOfRows);
     }
 }
