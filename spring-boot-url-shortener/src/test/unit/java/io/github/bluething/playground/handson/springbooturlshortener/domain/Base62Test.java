c958c8c3ff723c2a2ece4c66671c2fe3dddbd2b2
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-21 14:58:06 +0700

    Add failed test

---
+++ spring-boot-url-shortener/src/test/unit/java/io/github/bluething/playground/handson/springbooturlshortener/domain/Base62Test.java
@@ -0,0 +1,19 @@
+package io.github.bluething.playground.handson.springbooturlshortener.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class Base62Test {
+    private static Codec codec;
+
+    @BeforeAll
+    static void setup() {
+        codec = new Base62();
+    }
+
+    @Test
+    void givenLongValueReturnsStringWithBase62Encode() {
+        Assertions.assertEquals("zn9edcu", codec.encode(2009215674938L));
+    }
+}
