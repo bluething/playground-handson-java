f32e2a42477c23bb3b1d3a21f5b8f40e6376804f
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-21 22:57:42 +0700

    Add failed test

--- spring-boot-url-shortener/src/test/unit/java/io/github/bluething/playground/handson/springbooturlshortener/domain/UrlServiceTest.java
+++ spring-boot-url-shortener/src/test/unit/java/io/github/bluething/playground/handson/springbooturlshortener/domain/UrlServiceTest.java
@@ -1,5 +1,6 @@
 package io.github.bluething.playground.handson.springbooturlshortener.domain;
 
+import io.github.bluething.playground.handson.springbooturlshortener.infrastructure.persistence.UrlEntity;
 import io.github.bluething.playground.handson.springbooturlshortener.infrastructure.persistence.UrlRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +10,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UrlServiceTest {
@@ -31,5 +35,15 @@ class UrlServiceTest {
         given(codec.encode(2009215674938L)).willReturn("zn9edcu");
 
         Assertions.assertEquals("zn9edcu", urlService.generateShortenUrl("https://github.com/bluething"));
+    }
+
+    @DisplayName("Given database sequence call, when we call generateShortenUrl it must call save method to database")
+    @Test
+    void generateShortenUrlMustCallSaveToDatabaseMethod() {
+        given(urlRepository.getNextSequenceValue()).willReturn(2009215674938L);
+        given(codec.encode(2009215674938L)).willReturn("zn9edcu");
+        urlService.generateShortenUrl("https://github.com/bluething");
+        UrlEntity urlEntity = new UrlEntity(2009215674938L, "https://github.com/bluething", "zn9edcu");
+        verify(urlRepository, times(1)).save(urlEntity);
     }
 }
